7b896795ee6787e4a407278199448616fe91fb25
Bryan Moore <bryan.moore@comprend.com>
2017-03-15 22:25:09 +0100

    new code structure

---
+++ MusicalPancakeSite/MusicalPancakeSite/Controllers/ToDoListController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MusicalPancakeSite.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        public ActionResult ToDoListPage()
+        {
+            ViewBag.Message = "Hello world!";
+
+            return View();
+        }
+    }
+}
